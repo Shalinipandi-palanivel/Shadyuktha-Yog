7b0cd5b20a9ef2f6b894e5a31b577829d0bc0472
shalinipandi <shalinipandipalanivel17@gmail.com>
2026-03-05 20:40:08 +0530

    removed base

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,6 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-    base: '/Shadyuktha-Yog/', 
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
   alias: {
